fbd03a7d0704befac1b4701b7d4874c86f05cef5
TACQUET Sylvain <tacquet_s@HRM.dom>
2015-12-18 16:29:38 +0100

    third commit

--- DUMMY_TALEND/DUMMY_TALEND_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-ROOT_PATH=`pwd`
-java -Xms256M -Xmx1024M -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/log4j-1.2.16.jar:$ROOT_PATH/../lib/dom4j-1.6.1.jar:$ROOT_PATH/../lib/talendcsv.jar:$ROOT_PATH/dummy_talend_0_1.jar: hism.dummy_talend_0_1.DUMMY_TALEND --context=Default "$@" 
